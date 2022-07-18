b3be1ac63c2639e2cf6c5af68b66bcfff0861eb0
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-07-18 21:11:00 +0300

    Решена задача№1 Семинар№4 Число ПИ

--- Seminar4HW1.py
+++ Seminar4HW1.py
@@ -1,1 +1,13 @@
 #Задача№1 Семинар4
+
+# Вычислить число ПИ c заданной точностью d
+
+# Пример:
+
+# - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+import math
+from decimal import Decimal
+d = input("Введите число d = ")
+p = Decimal((math.pi)).quantize(Decimal(d))
+print("ПИ = :", p)
