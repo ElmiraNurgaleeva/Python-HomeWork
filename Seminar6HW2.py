e7c2473b24c7f0de97269a76a9147087748cfb2c
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-07-25 22:56:33 +0300

    Решена задача№2Семинар№6

---
+++ Seminar6HW2.py
@@ -0,0 +1,15 @@
+#Задача№2Семинар№6
+# Пример:[1,2,3,4] -> 4
+
+origin_list = [1, 2, 3, 4]
+
+
+def sum_odd_index(current_list):
+    length_current_list = len(current_list)
+    sum = 0
+    for i in range(0, length_current_list, 2):
+        sum += current_list[i]
+    return sum
+
+
+print(f'Сумма чисел списка {origin_list}, стоящих на нечетной позиции: {sum_odd_index(origin_list)}')
