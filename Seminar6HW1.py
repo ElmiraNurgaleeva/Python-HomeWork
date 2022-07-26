575ea258bb4baec63f392be1067344ac03387eba
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-07-26 14:07:42 +0300

    Добалено улучшенное решение задача№1 Семинар№6

--- Seminar6HW1.py
+++ Seminar6HW1.py
@@ -1,15 +1,29 @@
 #Задача№1 Семинар№6
 # Определить, присутствует ли в заданном списке строк, некоторое число
 
-str_list = ['Определить, присутствует ли', 'в заданном списке строк,', 'некоторое число (3)']
-present_flag = False
-n = 3
+# str_list = ['Определить, присутствует ли', 'в заданном списке строк,', 'некоторое число (3)']
+# present_flag = False
+# n = 3
 
-for i in str_list:
-    for j in i:
-        if j == 'n':
-            present_flag = True
-if present_flag:
-    print('Есть!')
-else:
-    print('Нет')
+# for i in str_list:
+#     for j in i:
+#         if j == 'n':
+#             present_flag = True
+# if present_flag:
+#     print('Есть!')
+# else:
+#     print('Нет')
+
+
+#2й вариант
+
+import numbers
+
+
+list = ['asd', 'a5k', '6', '50']
+
+def find (arr, n):
+    string = ''.join(arr)
+    return str(n) in string
+   
+print(find(list, 6))
