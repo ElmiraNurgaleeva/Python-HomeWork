421e5d06d713dc1d82c44a2752dbfadb6d883321
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-07-26 14:15:10 +0300

    Добалено улучшенное решение задача№2 Семинар№6

--- Seminar6HW2.py
+++ Seminar6HW2.py
@@ -2,15 +2,22 @@
 #Найти сумму чисел списка стоящих на нечетной позиции
 # Пример:[1,2,3,4] -> 4
 
-origin_list = [1, 2, 3, 4]
+# origin_list = [1, 2, 3, 4]
 
 
-def sum_odd_index(current_list):
-    length_current_list = len(current_list)
-    sum = 0
-    for i in range(0, length_current_list, 2):
-        sum += current_list[i]
-    return sum
+# def sum_odd_index(current_list):
+#     length_current_list = len(current_list)
+#     sum = 0
+#     for i in range(0, length_current_list, 2):
+#         sum += current_list[i]
+#     return sum
 
 
-print(f'Сумма чисел списка {origin_list}, стоящих на нечетной позиции: {sum_odd_index(origin_list)}')
+# print(f'Сумма чисел списка {origin_list}, стоящих на нечетной позиции: {sum_odd_index(origin_list)}')
+
+
+
+#2й вариант :
+
+n = int(input('n = '))
+print(sum(list(filter(lambda x: x%2, [x for x in range(1, n + 1)]))))
