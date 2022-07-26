721ee9b516b7299175902efbbce5fcbca15f0e44
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-07-26 14:33:04 +0300

    Добалено улучшенное решение задача№4 Семинар№6

--- Seminar6HW4.py
+++ Seminar6HW4.py
@@ -8,17 +8,38 @@
 # список: ["123", "234", 123, "567"], ищем: "123", ответ: -1
 # список: [], ищем: "123", ответ: -1
 
-import os
-os.system("cls")
-list = ["йцу", "фыв", "ячс", "цук", "йцукен", "йцу"]
-print(list)
-str = input('Что будем искать?: ')
-count = 0
-for i in range(len(list)):
-    if list[i] == str:
-        count += 1
-    if count == 2:
-        print('Позиция второго вхождения равна ', i)
-        break
-if count != 2:
-    print('-1')
+# import os
+# os.system("cls")
+# list = ["йцу", "фыв", "ячс", "цук", "йцукен", "йцу"]
+# print(list)
+# str = input('Что будем искать?: ')
+# count = 0
+# for i in range(len(list)):
+#     if list[i] == str:
+#         count += 1
+#     if count == 2:
+#         print('Позиция второго вхождения равна ', i)
+#         break
+# if count != 2:
+#     print('-1')
+
+
+#2й  вариант
+
+
+def find_second_match(where_list: list, to_find: str) -> int:
+    if len(list(filter(lambda x: x == to_find, where_list))) < 2:  
+        # второе совпадение не найдено
+        return -1
+    else:
+        return list(filter(lambda x: x[1] == to_find, list(enumerate(where_list))))[1][0]
+
+
+def main():
+    input_list = ["йцу", "фыв", "ячс", "цук", "йцукен", "йцу"]
+    search_string = "йцу"
+    print(find_second_match(input_list, search_string))
+
+
+if __name__ == "__main__":
+    main()
