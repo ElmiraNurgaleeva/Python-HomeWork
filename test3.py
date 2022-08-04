1200eaf611020959abe392a02618ec53a827a1ef
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-08-04 14:44:31 +0300

    Условие Задача Телефонный справочник

---
+++ test3.py
@@ -0,0 +1,9 @@
+# num = int(input("Введите число : "))
+# # print(num)
+# print("Второе число : ", num + 1)
+# print("Третье число : ", num - 1)
+
+num = int(input("Введите 1число : "))
+num2 = int(input("Введите 2число : "))
+num3 = int(input("Введите 3число : "))
+print("Сумма : ", num + num2 + num3)
