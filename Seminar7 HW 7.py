ab10d24855d92e372873dfa53f7c42c6766e9f9f
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-08-04 15:32:00 +0300

    Cеминар№7 п7

--- Seminar7 HW 7.py
+++ Seminar7 HW 7.py
@@ -3,5 +3,5 @@ def add_new():
     name = input("Insert new name: ")
     surname = input("Insert new surname: ")    
     phone_number = int(input("Insert new phone number: "))
-    return str([id, name, surname, phone_number]) 
+    return str([id, name, surname, phone_number] ) 
     
